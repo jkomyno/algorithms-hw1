226cbe9fafa33e1db4bb50ff3f97e3213d713638
Alberto Schiabel <skiabo97@gmail.com>
2020-04-04 22:59:18 +0200

    Added generic PriorityQueue with O(logN) update_key(k, v) operation based off of BinaryHeap

--- Shared/BinaryHeap.h
+++ Shared/BinaryHeap.h
@@ -1,28 +1,22 @@
 #pragma once
 
-#include <vector>    // std::vector
-#include <utility>   // std::forward
-#include <algorithm> // std::swap, std::find
-#include <optional>  // std::optional
-#include <cassert>   // std::assert
+#include <vector>     // std::vector
+#include <utility>    // std::forward
+#include <algorithm>  // std::swap
+#include <functional> // std::function
+#include <cassert>    // std::assert
 
 /**
- * Binary Heap implementation.
- * T is the type of the objects stored in the heap.
- * Compare is the type of a comparison function.
- * Use std::greater<> to obtain a Min Heap, use std::less<> to obtain a Max Heap.
- * In case a non-standard comparator is needed, it needs to be provided explicitly to
- * the constructor as 2nd argument.
+ * Generic Binary Heap implementation. Depending on the given comparator function, it can
+ * either be a Min Heap or a Max Heap.
+ *
+ * T: the type of the objects stored in the heap.
+ * is_heap_on_init: constexpr flag. If true, build_heap() won't be called after the class is constructed.
+ *                  If false, the given vector is considered an already valid heap.
  */
-template <typename T, class Compare = std::greater<>>
+template <typename T, bool is_heap_on_init = false>
 class BinaryHeap {
-    // keep the values in the heap
-    std::vector<T> nodes;
-
-    // comparison function
-    // std::greater<> -> Min Heap
-    // std::less<>    -> Max Heap
-    Compare comp;
+    using Compare = std::function<bool(T, T)>;
 
     // returns the parent of nodes[i]
     static size_t parent(size_t i) {
@@ -49,11 +43,29 @@ class BinaryHeap {
         return i >= bound;
     }
 
-    // create a Heap in O(n)
-    void build_heap() {
-        for (size_t i = (size() >> 1) + 1; i > 0; i--) {
-            heapify_down(i - 1);
+protected:
+    // protected constructor to let the subclass set the comparator
+    explicit BinaryHeap(std::vector<T>&& inputs) noexcept :
+        nodes(inputs) {
+        // spend O(n) to build the heap only if the given vector wasn't already a
+        // valid heap from the beginning
+        if constexpr (!is_heap_on_init) {
+            build_heap();
         }
+    }
+
+    // keep the values in the heap
+    std::vector<T> nodes;
+
+    // comparison function
+    // std::greater<> -> Min Heap
+    // std::less<>    -> Max Heap
+    Compare comp;
+
+    // swap 2 nodes in the heap
+    virtual void swap_nodes(size_t i, size_t j) {
+        using std::swap;
+        swap(nodes.at(i), nodes.at(j));
     }
 
     // starting from a Heap with a misplaced node at the given index,
@@ -66,6 +78,8 @@ class BinaryHeap {
             const size_t l = left(i);
             const size_t r = right(i);
 
+            // comp_est is the biggest element in a Max Heap,
+            // or the smallest element in a Min Heap
             size_t comp_est = i;
 
             if (l < len && comp(nodes.at(i), nodes.at(l))) {
@@ -79,9 +93,7 @@ class BinaryHeap {
                 return;
             }
 
-            using std::swap;
-            swap(nodes.at(i), nodes.at(comp_est));
-
+            swap_nodes(i, comp_est);
             i = comp_est;
         }
     }
@@ -90,31 +102,48 @@ class BinaryHeap {
     // recover the shape and heap properties in O(logn) bubbling the node up
     void heapify_up(size_t index_to_fix) {
         size_t i = index_to_fix;
-        using std::swap;
 
         size_t p = parent(i);
         while (i > 0 && comp(nodes.at(p), nodes.at(i))) {
-            swap(nodes.at(i), nodes.at(p));
+            swap_nodes(i, p);
             i = p;
+            p = parent(i);
         }
     }
 
 public:
     BinaryHeap() = delete;
 
-    explicit BinaryHeap(std::vector<T>&& inputs) noexcept :
-        nodes(inputs),
-        comp{} {
-        build_heap();
-    }
-
-    explicit BinaryHeap(std::vector<T>&& inputs, Compare&& comp) noexcept :
+    /**
+     * Compare is the type of a comparison function.
+     * Use std::greater<> to obtain a Min Heap, use std::less<> to obtain a Max Heap.
+     */
+    explicit BinaryHeap(std::vector<T>&& inputs, Compare comp) noexcept :
         nodes(inputs),
         comp(comp) {
-        build_heap();
+
+        // spend O(n) to build the heap only if the given vector wasn't already a
+        // valid heap from the beginning
+        if constexpr (!is_heap_on_init) {
+            build_heap();
+        }
     }
 
-    ~BinaryHeap() = default;
+    BinaryHeap(const BinaryHeap<T, is_heap_on_init>& rhs) = delete;
+    BinaryHeap& operator=(const BinaryHeap<T, is_heap_on_init>& rhs) = delete;
+
+    BinaryHeap(BinaryHeap<T, is_heap_on_init>&& rhs) = default;
+    BinaryHeap& operator=(BinaryHeap<T, is_heap_on_init>&& rhs) = default;
+
+    // virtual destructor because this is a base class
+    virtual ~BinaryHeap() = default;
+
+    // transform the internal vector in a heap in O(n)
+    void build_heap() {
+        for (size_t i = (size() >> 1) + 1; i > 0; i--) {
+            heapify_down(i - 1);
+        }
+    }
 
     // return the number of elements in the heap
     [[nodiscard]] size_t size() const {
@@ -128,14 +157,10 @@ public:
 
     // if BinaryHeap is instantiated as a Min Heap, returns the minimum element, otherwise
     // returns the maximum.
-    [[nodiscard]] const T& top() const {
+    [[nodiscard]] T top() const {
         assert(size() > 0);
 
         return nodes.at(0);
-    }
-
-    void reheap() {
-        build_heap();
     }
 
     // if BinaryHeap is instantiated as a Min Heap, removes the minimum element, otherwise
@@ -164,86 +189,17 @@ public:
         // the root violates the heap property. Let's fix that
         heapify_up(index_to_fix);
     }
-
-    /*
-    T extract_min() {
-        assert(size() > 0);
-
-        const T min = nodes.at(0);
-
-        // replace root of the heap with the last element of the vector
-        nodes.at(0) = nodes.back();
-
-        // remove the last element of the vector
-        nodes.pop_back();
-
-        // the root violates the heap property. Let's fix that
-        heapify_down(0);
-
-        return min;
-    }
-    
-    void insert(const T& node) {
-        const size_t new_index = size();
-
-        // insert new node at the end of the vector
-        nodes.emplace_back(node);
-        heapify_up(new_index);
-    }
-    */
-
-    /*
-    template <typename UnaryPredicate>
-    [[nodiscard]] std::optional<size_t> find(const UnaryPredicate& predicate) const {
-        const auto it = std::find_if(nodes.begin(), nodes.end(), predicate);
-        if (it != nodes.end()) {
-            size_t index = std::distance(nodes.begin(), it);
-            return std::optional{ index };
-        }
-        return std::nullopt;
-    }
-
-    T remove(const size_t k) {
-        const size_t len = size();
-
-        assert(len > 0);
-
-        // replace deleted node with the right-most leaf to keep
-        // the binary tree in shape
-        const auto last = nodes.back();
-
-        if (k == len - 1) {
-            nodes.pop_back();
-            return last;
-        }
-
-        nodes.pop_back();
-        auto result = nodes.at(k);
-        nodes.at(k) = last;
-
-        const size_t p = parent(k);
-
-        if (k == 0 || nodes.at(p) < nodes.at(k)) {
-            // move the k-th node down the tree
-            heapify_down(k);
-        } else {
-            // move the k-th node up the tree
-            heapify_up(k);
-        }
-
-        return result;
-    }
-    */
 };
+
 
 // create a Min Heap
 template <typename T>
-BinaryHeap<T, decltype(std::greater<>{})> make_min_heap(std::vector<T>&& inputs) {
-    return BinaryHeap<T, std::greater<>>(std::forward(inputs));
+BinaryHeap<T, false> make_min_heap(std::vector<T>&& inputs) {
+    return BinaryHeap<T, false>(std::forward(inputs), std::greater<>{});
 }
 
 // create a Max Heap
 template <typename T>
-BinaryHeap < T, decltype(std::less<>{}) > make_max_heap(std::vector<T>&& inputs) {
-    return BinaryHeap<T, std::less<>>(std::forward(inputs));
+BinaryHeap<T, false> make_max_heap(std::vector<T>&& inputs) {
+    return BinaryHeap<T, false>(std::forward(inputs), std::less<>{});
 }
---
+++ Shared/PriorityQueue.h
@@ -0,0 +1,181 @@
+#pragma once
+
+#include <unordered_map> // std::unordered_map
+#include <algorithm>     // std::swap, std::find
+#include <functional>    // std::function
+#include <cassert>       // std::assert
+#include "BinaryHeap.h"
+
+/**
+ * Priority Queue based on a Binary Heap.
+ *
+ * Key: type of the keys used to mantain the elements in a valid heap
+ * T: type of the elements stored in the priority queue
+ * is_heap_on_init: constexpr flag. If true, build_heap() won't be called after the class is constructed.
+ *                  If false, the given vector is considered an already valid heap.
+ * T_Hash: functor used to hash values of type T. Necessary only if T is a non-trivial type.
+ */
+template <typename Key, typename T, bool is_heap_on_init = false, typename T_Hash = std::hash<T>>
+class PriorityQueue : private BinaryHeap<T, is_heap_on_init> {
+
+    using super = BinaryHeap<T, is_heap_on_init>;
+    using key_map_type = std::unordered_map<T, Key, T_Hash>;
+    using index_map_type = std::unordered_map<T, size_t, T_Hash>;
+    using Compare = std::function<bool(T, T)>;
+    using CompareFactory = std::function<Compare(key_map_type&)>;
+
+    // keep track of the value of the keys of each node.
+    key_map_type key_map;
+
+    // keep track of the index of the nodes
+    index_map_type index_map;
+
+    // initialize the key map
+    template <typename T, typename Key, typename T_Hash = std::hash<T>>
+    static std::unordered_map<size_t, Key, T_Hash> build_key_map(std::vector<Key>&& keys, std::vector<T>& node_list) {
+        assert(keys.size() == node_list.size());
+
+        std::unordered_map<T, Key, T_Hash> local_key_map(node_list.size());
+
+        size_t index = 0;
+        for (const auto& node : node_list) {
+            local_key_map[node] = keys[index];
+            index++;
+        }
+
+        return local_key_map;
+    }
+
+    // initialize index map
+    template <typename T, typename Key, typename T_Hash = std::hash<T>>
+    static std::unordered_map<T, size_t, T_Hash> build_index_map(std::vector<T>& node_list) {
+        std::unordered_map<T, size_t, T_Hash> local_index_map(node_list.size());
+
+        size_t index = 0;
+        for (const auto& node : node_list) {
+            local_index_map[node] = index;
+            index++;
+        }
+
+        return local_index_map;
+    }
+
+protected:
+    void swap_nodes(size_t i, size_t j) override {
+        using std::swap;
+
+        auto node_i = this->nodes.at(i);
+        auto node_j = this->nodes.at(j);
+
+        // swap(key_map.at(node_i), key_map.at(node_j));
+        swap(index_map.at(node_i), index_map.at(node_j));
+        super::swap_nodes(i, j);
+    }
+
+public:
+    PriorityQueue() = delete;
+
+    explicit PriorityQueue(std::vector<Key>&& keys, std::vector<T>&& inputs, CompareFactory&& comp) :
+        super(std::move(inputs)),
+        key_map(build_key_map<T, Key, T_Hash>(std::move(keys), this->nodes)),
+        index_map(build_index_map<T, size_t, T_Hash>(this->nodes)) {
+        this->comp = comp(key_map);
+        this->build_heap();
+    }
+
+    explicit PriorityQueue(const PriorityQueue<Key, T>& rhs) = delete;
+    PriorityQueue& operator=(const PriorityQueue<Key, T>& rhs) = delete;
+
+    ~PriorityQueue() = default;
+
+    // return the number of elements in the heap
+    [[nodiscard]] size_t size() const {
+        return super::size();
+    }
+
+    // return true iff the heap is empty
+    [[nodiscard]] bool empty() const {
+        return super::empty();
+    }
+
+    // adds a new element to the heap
+    void push(const Key& key, const T& node) {
+        const size_t index_to_fix = this->size();
+        index_map[node] = index_to_fix;
+        key_map[node] = key;
+
+        super::push(node);
+    }
+
+    // update the key of a node in the priority queue.
+    // node must exist in the priority queue
+    void update_key(const Key& key, const T& node) {
+
+        const size_t index_to_fix = index_map[node];
+        key_map[node] = key;
+
+        // recover the heap property
+        this->heapify_up(index_to_fix);
+    }
+
+    // returns the value of the key of the given node
+    [[nodiscard]] const Key& key_at(const T& node) const {
+        return key_map.at(node);
+    }
+
+    // returns true iff the given node is in the priority queue
+    [[nodiscard]] bool contains(const T& node) const {
+        const auto search = index_map.find(node);
+        return search != index_map.end();
+    }
+
+    // return the top element
+    [[nodiscard]] T top() const {
+        return super::top();
+    }
+
+    // remove the top element
+    void pop() {
+        assert(this->size() > 0);
+
+        const size_t index_to_remove = 0;
+        const auto node = this->nodes.at(index_to_remove);
+        index_map.erase(node);
+        key_map.erase(node);
+
+        // replace root of the heap with the last element of the vector
+        this->nodes.at(0) = this->nodes.back();
+
+        // remove the last element of the vector
+        this->nodes.pop_back();
+
+        // no need to recover the heap property if the heap is empty
+        if (this->size() > 0) {
+            const auto front = this->nodes.front();
+            index_map[front] = 0;
+
+            // the root violates the heap property. Let's fix that
+            this->heapify_down(0);
+        }
+    }
+};
+
+// create a Priority Queue based on a Min Heap
+template <typename Key, typename Value, bool is_heap_on_init = false>
+PriorityQueue<Key, Value, is_heap_on_init> make_min_priority_queue(std::vector<Key>&& keys, std::vector<Value>&& inputs) {
+    return PriorityQueue<Key, Value, is_heap_on_init>(std::move(keys), std::move(inputs), [](auto& key_map) {
+        return [&key_map](auto&& a, auto&& b) -> bool {
+            return key_map.at(a) > key_map.at(b);
+        };
+    });
+}
+
+// create a Priority Queue based on a Max Heap
+template <typename Key, typename Value, bool is_heap_on_init = false>
+PriorityQueue<Key, Value, is_heap_on_init> make_max_priority_queue(std::vector<Key>&& keys, std::vector<Value>&& inputs) {
+    return PriorityQueue<Key, Value, is_heap_on_init>(std::move(keys), std::move(inputs), [](auto& key_map) {
+        return [&key_map](auto&& a, auto&& b) -> bool {
+            return key_map.at(a) < key_map.at(b);
+        };
+    });
+}
