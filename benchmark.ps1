cbaa08ebec9e25bcdda7a76e4a7fbdf73e68344d
Alberto Schiabel <skiabo97@gmail.com>
2020-04-05 18:16:14 +0200

    Added benchmark.ps1 and testall.ps1 scripts

---
+++ benchmark.ps1
@@ -0,0 +1,14 @@
+# Usage: ./benchmark.ps1
+
+$exe_folder = ".\x64\Release"
+$programs = "PrimBinaryHeap","KruskalUnionFind","KruskalSimple"
+$output_folder = ".\benchmark"
+
+foreach ($program in $programs) {
+  $timestamp = $(get-date -f "yyyyMMdd_HHmmss_ff")
+  $output_filename = "$program.$timestamp.csv"
+  echo "Benchmarking $program"
+  (& .\time.ps1 "$exe_folder\$program" "datafake/") | Out-String | Tee-Object -variable out
+
+  echo $out > "$output_folder\$output_filename"
+}
---
+++ testall.ps1
@@ -0,0 +1,15 @@
+# Usage: ./testall.ps1
+
+$exe_folder = ".\x64\Release"
+$programs = "KruskalSimple","KruskalUnionFind","PrimBinaryHeap"
+
+foreach ($program in $programs) {
+  echo "Testing $program"
+  & { Invoke-Expression ".\test.ps1 $exe_folder\$program" } 2>&1 | Tee-Object -Variable out_content
+  echo ""
+}
+
+# Run tests in parallel
+# $programs | ForEach-Object {
+#   Start-Job -Init ([ScriptBlock]::Create("Set-Location '$pwd'")) -FilePath .\test.ps1 -ArgumentList "$exe_folder\$program"
+# } | Wait-Job | Receive-Job
