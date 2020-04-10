4b7d27772f538b1700e1d9148a270c684d78af43
Alberto Schiabel <skiabo97@gmail.com>
2020-04-11 00:28:07 +0200

    Refactored benchmark.ps1 and time.ps1 scripts to have more real-time info about the input file being processed

--- time.ps1
+++ time.ps1
@@ -1,4 +1,4 @@
-# Usage: ./time.ps1 [program-name] [dataset-prefix] > outputs.csv
+# Usage: ./time.ps1 [program-name] [input-file] > outputs.csv
 # create a UTF-16 encoded CSV file that can be easily processed by Python
 #
 # The CSV file has the following columns
@@ -8,22 +8,16 @@
 # m: number of graph edges
 # filename: name of the input file that contained the graph definition
 
-echo "ms;output;n;m;filename"
-
 $program = $args[0]
-$prefix = $args[1]
+$input_file = $args[1]
 
-Get-ChildItem "./" -Filter "$prefix*.txt" |
-Foreach-Object {
-    $filename = $_.FullName
-    $basename = $_.Basename
+$filename = [System.IO.Path]::GetFileName("$input_file")
 
-    $first_line = Get-Content $filename -First 1
-    $n,$m = $first_line.split(' ')
+$first_line = Get-Content $input_file -First 1
+$n,$m = $first_line.split(' ')
 
-    $ms = Measure-Command {
-      $output = (Get-Content $filename | & $program)
-    } | Select -ExpandProperty TotalMilliseconds
+$ms = Measure-Command {
+  $output = (Get-Content $input_file | & $program)
+} | Select -ExpandProperty TotalMilliseconds
 
-    echo "$ms;$output;$n;$m;$basename"
-}
+echo "$ms;$output;$n;$m;$filename"
