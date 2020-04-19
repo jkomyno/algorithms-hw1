c603d761c2eb6f7e3355fbac0ab70fbfa3547f60
Alberto Schiabel <skiabo97@gmail.com>
2020-04-19 15:37:21 +0200

    Refactored DisjointSetCompressed to implement union-by-rank with path-splitting compression

--- Shared/DisjointSet.h
+++ Shared/DisjointSet.h
@@ -16,10 +16,14 @@ template <typename T, typename = typename std::enable_if<
 class DisjointSet : public DisjointSetBase<T> {
 	using super = DisjointSetBase<T>;
 
+	// vector that keeps track of the size of every element in the DisjointSet
+	std::vector<size_t> sizes;
 public:
 	// x_list must contain unsigned integers of distinct value in the range [0, x_list.size())
 	explicit DisjointSet(std::vector<T>&& x_list) :
-		super(std::move(x_list)) { }
+		super(std::move(x_list)),
+		sizes(x_list.size(), 1) // fill size with value 1
+	{ }
 
 	// default move constructor and assignment operator
 	DisjointSet(DisjointSet&& disjoint_set) noexcept = default;
@@ -29,11 +33,8 @@ public:
 	DisjointSet(const DisjointSet& rhs) = delete;
 	DisjointSet& operator=(const DisjointSet& rhs) = delete;
 
-	// default destructor
-	~DisjointSet() = default;
-
 	/**
-	 * returns the index of the representative of the unique set containing the given item.
+	 * Returns the index of the representative of the unique set containing the given item.
 	 * T must be castable to size_t.
 	 * O(logn)
 	 */
@@ -41,10 +42,40 @@ public:
 		auto x = element;
 		
 		while (x != this->parents[x]) {
-			// move to parent
+			// move towards the parent
 			x = this->parents[x];
 		}
 		return x;
+	}
+
+	/**
+	 * Unites the dynamic sets that contain x and y into a new set that is the union
+	 * of these two sets.
+	 * x should be different than y.
+	 * Uses union-by-size policty: it makes the node whose set size is smaller point to
+	 * the node whose set size is bigger, breaking ties arbitrarily.
+	 * The complexity of this method is the one of find + O(1)
+	 */
+	void unite(const T& x, const T& y) noexcept override {
+		const size_t i = find(x);
+		const size_t j = find(y);
+
+		// if the two nodes are the same set, do nothing
+		if (i != j) {
+			if (this->sizes[i] < this->sizes[j]) {
+				this->parents[i] = j;
+				this->sizes[j] += this->sizes[i];
+			} else {
+				this->parents[j] = i;
+				this->sizes[i] += this->sizes[j];
+			}
+		}
+	}
+
+	auto get_rank_updates_comparisons() const noexcept {
+		auto max_rank = std::max_element(this->sizes.begin(), this->sizes.end());
+		auto min_rank = std::min_element(this->sizes.begin(), this->sizes.end());
+		return std::pair(*max_rank, *min_rank);
 	}
 };
 
--- Shared/DisjointSetCompressed.h
+++ Shared/DisjointSetCompressed.h
@@ -4,12 +4,13 @@
 #include <functional>  // std::function
 #include <type_traits> // std::enable_if, std::is_unsigned
 #include <vector>      // std::vector
+#include <algorithm>
 
 #include "DisjointSetBase.h"
 
 /**
- * Simple union-by-size implementation of a Disjoint-Set.
- * DisjointSet accepts element of type T, which must be an unsigned integer type.
+ * Compressed union-by-rank implementation of a Disjoint-Set.
+ * DisjointSetCompressed accepts element of type T, which must be an unsigned integer type.
  */
 template <typename T, typename = typename std::enable_if<
 	std::is_unsigned<T>::value>::type
@@ -17,10 +18,14 @@ template <typename T, typename = typename std::enable_if<
 class DisjointSetCompressed : public DisjointSetBase<T> {
 	using super = DisjointSetBase<T>;
 
+	// vector that keeps track of the rank of every element in the DisjointSet
+	std::vector<size_t> ranks;
 public:
 	// x_list must contain unsigned integers of distinct value in the range [0, x_list.size())
 	explicit DisjointSetCompressed(std::vector<T>&& x_list) :
-		super(std::move(x_list)) { }
+		super(std::move(x_list)),
+		ranks(x_list.size(), 0) // fill size with value 0
+	{ }
 
 	// default move constructor and assignment operator
 	DisjointSetCompressed(DisjointSetCompressed&& disjoint_set) noexcept = default;
@@ -30,30 +35,59 @@ public:
 	DisjointSetCompressed(const DisjointSetCompressed& rhs) = delete;
 	DisjointSetCompressed& operator=(const DisjointSetCompressed& rhs) = delete;
 
-	// default destructor
-	~DisjointSetCompressed() = default;
-
 	/**
 	 * Returns the index of the representative of the unique set containing the given item.
 	 * T must be castable to size_t.
-	 * find implements path compression via path halving: every time find is called on a node, it
+	 * find implements path compression via path splitting: every time find is called on a node, it
 	 * makes every node in the path point to its grandparent, effectively making the trees
 	 * flat or almost flat.
-	 * O(n + lg*(n)) (basically linear, lg*(265536) == 5)
+	 * O(lg*(n)) (basically linear, lg*(265536) == 5)
 	 */
 	size_t find(const T& element) noexcept override {
 		auto x = element;
-		auto& parents = this->parents;
-
-		while (x != parents[x]) {
-			// skip parent
-			parents[x] = parents[parents[x]];
+		
+		while (x != this->parents[x]) {
+			// skip parent and point to grandparent
+			this->parents[x] = this->parents[this->parents[x]];
 
 			// move to grandparent
-			x = parents[x];
+			x = this->parents[x];
 		}
 
 		return x;
+	}
+
+	/**
+	 * Unites the dynamic sets that contain x and y into a new set that is the union
+	 * of these two sets.
+	 * x should be different than y.
+	 * Uses union-by-rank policty: it makes the node whose set rank is smaller point to
+	 * the node whose set size is bigger, breaking ties arbitrarily. In case of tie,
+	 * the resulting set's rank is increased by 1.
+	 * The complexity of this method is the one of find + O(1)
+	 */
+	void unite(const T& x, const T& y) noexcept override {
+		auto i = find(x);
+		auto j = find(y);
+
+		if (i != j) {
+			// if two sets are united and have different ranks,
+			// the resulting set's rank is the larger of the two
+			if (this->ranks[i] < this->ranks[j]) {
+				using std::swap;
+				swap(i, j);
+			}
+
+			this->parents[j] = i;
+
+			if (this->ranks[i] != this->ranks[j]) {
+				return;
+			}
+
+			// if two sets are united and have the same rank, the resulting
+			// set's rank is one larger
+			++(this->ranks[i]);
+		}
 	}
 };
 
