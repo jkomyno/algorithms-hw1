bb2fd827680714a7e9a2b86e061bde5bfe9855ef
Alberto Schiabel <skiabo97@gmail.com>
2020-04-24 19:38:15 +0200

    Regenerated project PrimBinaryHeap (Visual Studio was stuck with SDK 8.1 instead of SDK 10, resulting in compilation issues)

--- PrimBinaryHeap/main.cpp
+++ PrimBinaryHeap/main.cpp
@@ -6,17 +6,17 @@
 #include "prim_binary_heap_mst.h"
 
 int main() noexcept {
-    typedef size_t Label; // nodes are identified by size_t type
-    typedef long Weight;  // weights are of type long
+	typedef size_t Label; // nodes are identified by size_t type
+	typedef long Weight;  // weights are of type long
 
 	AdjacencyMapGraph<Label, Weight> adj_map_graph(adjacency_map_graph_factory<Label, Weight>());
 
-    // compute Minimum Spanning Tree with Prim algorithm using Binary Heap
-    const auto& mst = prim_binary_heap_mst(std::move(adj_map_graph));
+	// compute Minimum Spanning Tree with Prim algorithm using Binary Heap
+	const auto& mst = prim_binary_heap_mst(std::move(adj_map_graph));
 
-    // total weight of the mst found by Prim algorithm with Binary Heap
-    const auto total_weight = sum_weights<Label, Weight>(mst.cbegin(), mst.cend());
+	// total weight of the mst found by Prim algorithm with Binary Heap
+	const auto total_weight = sum_weights<Label, Weight>(mst.cbegin(), mst.cend());
 
-    // use std::fixed to avoid displaying numbers in scientific notation
-    std::cout << std::fixed << total_weight << std::endl;
+	// use std::fixed to avoid displaying numbers in scientific notation
+	std::cout << std::fixed << total_weight << std::endl;
 }
--- PrimBinaryHeap/prim_binary_heap_mst.h
+++ PrimBinaryHeap/prim_binary_heap_mst.h
@@ -10,49 +10,49 @@
 
 template <typename Label, typename Weight>
 std::vector<Edge<Label, Weight>> prim_binary_heap_mst(AdjacencyMapGraph<Label, Weight>&& adj_map_graph) noexcept {
-    auto vertexes = adj_map_graph.get_vertexes();
+	auto vertexes = adj_map_graph.get_vertexes();
 
-    const size_t n_stop = vertexes.size();
-    std::vector<Edge<Label, Weight>> mst(n_stop);
+	const size_t n_stop = vertexes.size();
+	std::vector<Edge<Label, Weight>> mst(n_stop);
 
-    // Keys are used to pick the lightest edge in cut.
-    // Initially, keys of all vertexes are set to infinity.
-    std::vector<Weight> keys;
-    keys.reserve(vertexes.size());
-    std::transform(vertexes.cbegin(), vertexes.cend(), std::back_inserter(keys), [](const auto&) {
-        return std::numeric_limits<Weight>::max();
-    });
+	// Keys are used to pick the lightest edge in cut.
+	// Initially, keys of all vertexes are set to infinity.
+	std::vector<Weight> keys;
+	keys.reserve(vertexes.size());
+	std::transform(vertexes.cbegin(), vertexes.cend(), std::back_inserter(keys), [](const auto&) {
+		return std::numeric_limits<Weight>::max();
+		});
 
-    // the source vertex can be randomly chosen. For simplicity, we choose the first vertex available.
-    // the first vertex is distant 0 from itself
-    keys.at(0) = Weight(0);
+	// the source vertex can be randomly chosen. For simplicity, we choose the first vertex available.
+	// the first vertex is distant 0 from itself
+	keys.at(0) = Weight(0);
 
-    // Priority Queue based on a Min Heap ordered by keys, from smaller to larger.
-    // We don't need the O(N) first to reorder the vertexes as a heap, since keys is already
-    // a valid heap. The priority queue creation thus takes O(1) time
-    constexpr bool is_heap_on_init = true;
-    auto priority_queue(make_min_priority_queue<Weight, Label, is_heap_on_init>(std::move(keys), std::move(vertexes)));
+	// Priority Queue based on a Min Heap ordered by keys, from smaller to larger.
+	// We don't need the O(N) first to reorder the vertexes as a heap, since keys is already
+	// a valid heap. The priority queue creation thus takes O(1) time
+	constexpr bool IsAlreadyHeap = true;
+	auto min_pq(priority_queue::make_min_priority_queue<IsAlreadyHeap>(std::move(keys), std::move(vertexes)));
 
-    while (!priority_queue.empty()) {
-        // u is the vertex with minimum key that belongs to the lightest edge of the cut
-        auto u = priority_queue.top();
-        priority_queue.pop();
+	while (!min_pq.empty()) {
+		// u is the vertex with minimum key that belongs to the lightest edge of the cut
+		auto u = min_pq.top();
+		min_pq.pop();
 
-        // traverse all vertexes which are adjacent to u
-        for (const auto [v, weight] : adj_map_graph.adjacent_vertexes(u)) {
-            // if v is not in MST and w(u, v) is smaller than the current key of v
-            if (priority_queue.contains(v) && weight < priority_queue.key_at(v)) {
-                // update the key associated with node v in O(logN), where N is the number
-                // of elements stored in the priority queue
-                priority_queue.update_key(weight, v);
+		// traverse all vertexes which are adjacent to u
+		for (const auto [v, weight] : adj_map_graph.adjacent_vertexes(u)) {
+			// if v is not in MST and w(u, v) is smaller than the current key of v
+			if (min_pq.contains(v) && weight < min_pq.key_at(v)) {
+				// update the key associated with node v in O(logN), where N is the number
+				// of elements stored in the priority queue
+				min_pq.update_key(weight, v);
 
-                // add the edge (u, v) to the Minimum Spanning Tree
-                mst.at(v) = Edge<Label, Weight>(u, v, weight);
-            }
-        }
-    }
+				// add the edge (u, v) to the Minimum Spanning Tree
+				mst.at(v) = Edge<Label, Weight>(u, v, weight);
+			}
+		}
+	}
 
-    return mst;
+	return mst;
 }
 
 #endif  // PRIM_BINARY_HEAP_MST_H
